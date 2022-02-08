2245dd5888f7294657b2e6faf2caeb18ab1623fb
gkaranikolas <gkaranikolas@arx.net>
2022-02-08 17:48:25 +0200

    Add Custom OutlinedTextField

--- app/src/main/java/eu/seijindemon/mywidget/ui/composable/SettingsScreen.kt
+++ app/src/main/java/eu/seijindemon/mywidget/ui/composable/SettingsScreen.kt
@@ -2,14 +2,23 @@ package eu.seijindemon.mywidget.ui.composable
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -44,6 +53,77 @@ fun SettingsContent(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
+        Divider(modifier = Modifier.height(16.dp))
+
+    }
+}
+
+@Composable
+fun CustomOutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String = "",
+    leadingIconImageVector: ImageVector,
+    leadingIconDescription: String = "",
+    isPasswordField: Boolean = false,
+    isPasswordVisible: Boolean = false,
+    onVisibilityChange: (Boolean) -> Unit = {},
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    showError: Boolean = false,
+    errorMessage: String = ""
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp),
+            label = { Text(text = label) },
+            leadingIcon = {
+                Icon(
+                    imageVector = leadingIconImageVector,
+                    contentDescription = leadingIconDescription,
+                    tint = if (showError) MaterialTheme.colors.error else MaterialTheme.colors.onSurface
+                )
+            },
+            isError = showError,
+            trailingIcon = {
+                if (showError && !isPasswordField) Icon(imageVector = Icons.Filled.Error, contentDescription = "Show error icon")
+                if (isPasswordField) {
+                    IconButton(onClick = { onVisibilityChange(!isPasswordVisible) }) {
+                        Icon(
+                            imageVector = if (isPasswordField) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                            contentDescription = "Toggle password visibility"
+                        )
+                    }
+                }
+            },
+            visualTransformation = when {
+                isPasswordField && isPasswordVisible -> VisualTransformation.None
+                isPasswordField -> PasswordVisualTransformation()
+                else -> VisualTransformation.None
+            },
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            singleLine = true
+        )
+        if (showError) {
+            Text(
+                text = errorMessage,
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .offset(y = (-8).dp)
+                    .fillMaxWidth(0.9f)
+            )
+        }
     }
 }
 
